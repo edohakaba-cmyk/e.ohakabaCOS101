d2c4438db6d644855c851215866193b3eeb4889d
david <ed.ohakaba@gmail.com>
2025-11-12 23:09:58 +0100

    Convert week folders from embedded repos to normal folders

---
+++ week-2/practice_1.rs
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+fn main(){
+println!("Welcome to COS 101");
+
+println! ("The course learning outcome is as follows:");
+
+println!("1. Distinguish between computer and computer programming,/n
+	2. Develop some techniques in computer science,/n
+	3. Understand the different areas of study in computer science,/n
+	4. Conversant with applications of computer science,/n
+	5. Navigate the carrer prospects in computer science./n
+	6. Conversant with computer programming concepts." );
+
+
+
+
+
+
+
+
+=======
+fn main(){
+println!("Welcome to COS 101");
+
+println! ("The course learning outcome is as follows:");
+
+println!("1. Distinguish between computer and computer programming,/n
+	2. Develop some techniques in computer science,/n
+	3. Understand the different areas of study in computer science,/n
+	4. Conversant with applications of computer science,/n
+	5. Navigate the carrer prospects in computer science./n
+	6. Conversant with computer programming concepts." );
+
+
+
+
+
+
+
+
+>>>>>>> 86dcf0cecb49c4558f670204d1d94708732dfd34
+}
---
+++ week-2/project_1.rs
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+fn  main() {
+	let p:f64=520_000_000.0;
+	let r:f64=10.0;
+	let n:f64=5.0;
+
+	// compound intrest calculator
+	let a=p*(1.0+(r/100.0)).powf (n);
+	println! ("amount ={}",a);
+	let ci=a-p;
+	println!("compound intrest of Ibeju Local Goverment is {}", ci);
+}
+=======
+fn  main() {
+	let p:f64=520_000_000.0;
+	let r:f64=10.0;
+	let n:f64=5.0;
+
+	// compound intrest calculator
+	let a=p*(1.0+(r/100.0)).powf (n);
+	println! ("amount ={}",a);
+	let ci=a-p;
+	println!("compound intrest of Ibeju Local Goverment is {}", ci);
+}
+>>>>>>> 86dcf0cecb49c4558f670204d1d94708732dfd34
