6f0c466d3cb4d25efd767ae1d75b2ef387279934
david <ed.ohakaba@gmail.com>
2025-11-19 22:40:22 +0100

    practice 1 assignment submit

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,14 @@
+fn main(){
+
+    //using Vec::new()
+    let v : Vec<i64> = Vec::new();
+
+    //   printing the size of the vector
+    println!("\nThe length of vec::new is: {}",v. len());
+
+    //Using marco
+    let v = vec!["Grace","Effiong", "Basil", "Kareem","susan"];
+
+    //printing the size of vector
+    println!("\nThe length of vec marco is: { }",v.len());
+}
