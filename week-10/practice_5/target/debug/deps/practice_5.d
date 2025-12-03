9e95c5b69e28e3ef52b9e4d9f9efa6669ff334c4
david <ed.ohakaba@gmail.com>
2025-12-03 18:49:06 +0100

    borrowing in rust

---
+++ week-10/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\HP LAPTOP\Documents\e.ohakabaCOS101\week-10\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\HP LAPTOP\Documents\e.ohakabaCOS101\week-10\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
